6334f0c8f94609fe0b873b634f982100cbadf321
Vishal Anand <vishal.anand@qtsolv.com>
2022-03-27 20:41:35 +0530

    test2

--- movies-server/__test__/movies.test.js
+++ movies-server/__test__/movies.test.js
@@ -1,17 +1,16 @@
-const request = require('supertest');
-const app = require('../src/app');
+const request = require("supertest");
+const app = require("../src/app");
 
 describe("get /test", () => {
-    describe("testing the test", () => {
-        
-        test("response from test", async () => {
-            const response = await request(app).get("/test");
-            expect(response.status).toBe(200);
-        })
+  describe("testing the test", () => {
+    test("response from test", async () => {
+      const response = await request(app).get("/test");
+      expect(response.status).toBe(200);
+    });
 
-        test("testing env variable ", async () => {
-            const response = await request(app).get("/test2");
-            expect(response.status).toBe(200);
-        })
-    })
-})
+    test("testing env variable ", async () => {
+      const response = await request(app).get("/test2");
+      expect(response.status).toBe(500);
+    });
+  });
+});
