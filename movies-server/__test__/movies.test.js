0370fe87307129ec450f641be24aff15aedaa8c1
Vishal Anand <vishal.anand@qtsolv.com>
2022-03-27 23:50:42 +0530

    changes4444

--- movies-server/__test__/movies.test.js
+++ movies-server/__test__/movies.test.js
@@ -29,26 +29,55 @@ const mongoose = require("mongoose");
 //   });
 // });
 
-let mongo = "";
-beforeAll(async () => {
-  process.env.API_KEY = "2c46c474";
-  mongo = await MongoMemoryServer.create();
-  const mongoUri = await mongo.getUri();
+// let mongo = "";
+// beforeAll(async () => {
+//   process.env.API_KEY = "2c46c474";
+//   mongo = await MongoMemoryServer.create();
+//   const mongoUri = await mongo.getUri();
 
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+//   await mongoose.connect(mongoUri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+// });
+
+// afterAll(async () => {
+//   await mongo.stop();
+//   await mongoose.connection.close();
+// });
+
+// test("Should create a movie for authorized user with movie title", async () => {
+//   const response = await request(app).post("/movies").send({
+//     movieTitle: "No entry",
+//   });
+//   expect(response.statusCode).toBe(200);
+// });
+
+const { MongoClient } = require("mongodb");
+
+describe("insert", () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+    });
+    db = await connection.db(global.__MONGO_DB_NAME__);
   });
-});
 
-afterAll(async () => {
-  await mongo.stop();
-  await mongoose.connection.close();
-});
-
-test("Should create a movie for authorized user with movie title", async () => {
-  const response = await request(app).post("/movies").send({
-    movieTitle: "No entry",
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
   });
-  expect(response.statusCode).toBe(200);
+
+  it("should insert a doc into collection", async () => {
+    const users = db.collection("users");
+
+    const mockUser = { _id: "some-user-id", name: "John" };
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({ _id: "some-user-id" });
+    expect(insertedUser).toEqual(mockUser);
+  });
 });
