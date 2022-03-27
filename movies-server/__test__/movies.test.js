87990368c31da43294104181d1a5823988985fbd
Vishal Anand <vishal.anand@qtsolv.com>
2022-03-28 00:31:28 +0530

    ggggg

--- movies-server/__test__/movies.test.js
+++ movies-server/__test__/movies.test.js
@@ -3,74 +3,14 @@ const app = require("../src/app");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const mongoose = require("mongoose");
 
-// describe("create movies for users", () => {
-//   let mongo = "";
-//   beforeAll(async () => {
-//     process.env.API_KEY = "2c46c474";
-//     mongo = await MongoMemoryServer.create();
-//     const mongoUri = await mongo.getUri();
-
-//     await mongoose.connect(mongoUri, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//   });
-
-//   afterAll(async () => {
-//     await mongo.stop();
-//     await mongoose.connection.close();
-//   });
-
-//   test("Should create a movie for authorized user with movie title", async () => {
-//     const response = await request(app).post("/movies").send({
-//       movieTitle: "No entry",
-//     });
-//     expect(response.statusCode).toBe(200);
-//   });
-// });
-
-// let mongo = "";
-// beforeAll(async () => {
-//   process.env.API_KEY = "2c46c474";
-//   mongo = await MongoMemoryServer.create();
-//   const mongoUri = await mongo.getUri();
-
-//   await mongoose.connect(mongoUri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
-// });
-
-// afterAll(async () => {
-//   await mongo.stop();
-//   await mongoose.connection.close();
-// });
-
-// test("Should create a movie for authorized user with movie title", async () => {
-//   const response = await request(app).post("/movies").send({
-//     movieTitle: "No entry",
-//   });
-//   expect(response.statusCode).toBe(200);
-// });
-
-const { MongoClient } = require("mongodb");
-
-describe("insert", () => {
-  let connection;
-  let db;
-
+describe("create movies for users", () => {
   beforeAll(async () => {
-    connection = await MongoClient.connect(global.__MONGO_URI__, {
-      useNewUrlParser: true,
-    });
-    db = await connection.db(global.__MONGO_DB_NAME__);
+    process.env.API_KEY = "2c46c474";
+    process.env.MONGO_URI =
+      "mongodb+srv://vishal:vishal@cluster0.tuv3n.mongodb.net/nodeJsTask?retryWrites=true&w=majority";
   });
 
-  afterAll(async () => {
-    await connection.close();
-  });
-
-  it("Should create a movie for authorized user with movie title", async () => {
+  test("Should create a movie for authorized user with movie title", async () => {
     const response = await request(app).post("/movies").send({
       movieTitle: "No entry",
     });
