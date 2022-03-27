8b7af85e9f542d1128b561f4a1e13e92d7603aeb
Vishal Anand <vishal.anand@qtsolv.com>
2022-03-28 00:46:57 +0530

    code

--- movies-server/__test__/movies.test.js
+++ movies-server/__test__/movies.test.js
@@ -1,19 +1,48 @@
 const request = require("supertest");
 const app = require("../src/app");
 const { MongoMemoryServer } = require("mongodb-memory-server");
+const Movi = require("../src/models/movies");
 const mongoose = require("mongoose");
 
 describe("create movies for users", () => {
   beforeAll(async () => {
     process.env.API_KEY = "2c46c474";
-    process.env.MONGO_URI =
-      "mongodb+srv://vishal:vishal@cluster0.tuv3n.mongodb.net/nodeJsTask?retryWrites=true&w=majority";
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = await mongo.getUri();
+
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+
+  afterAll(async () => {
+    await mongo.stop();
+    await mongoose.connection.close();
   });
 
   test("Should create a movie for authorized user with movie title", async () => {
-    const response = await request(app).post("/movies").send({
-      movieTitle: "No entry",
+    const movi = new Movi({
+      userId: "123",
+      title: "no entry",
+      released: "1222",
+      genre: "comedey",
+      director: "test",
+      createdAt: "111100020202",
+      month: "3",
+      year: "2022",
     });
-    expect(response.statusCode).toBe(200);
+    await movi.save();
+
+    await request(app)
+      .post("/movies")
+      .send({ movieTitle: "no entry" })
+      .expect(response.statusCode)
+      .toBe(200);
+
+    // const response = await request(app).post("/movies").send({
+    //   movieTitle: "No entry",
+    // });
+    // expect(response.statusCode).toBe(200);
   });
 });
