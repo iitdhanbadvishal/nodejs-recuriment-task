77db9b058f631484f1fcb56fb992ef6bed16d8d3
Vishal Anand <vishal.anand@qtsolv.com>
2022-03-27 23:43:16 +0530

    changes

--- movies-server/__test__/movies.test.js
+++ movies-server/__test__/movies.test.js
@@ -3,28 +3,52 @@ const app = require("../src/app");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const mongoose = require("mongoose");
 
-describe("create movies for users", () => {
-  let mongo = "";
-  beforeAll(async () => {
-    process.env.API_KEY = "2c46c474";
-    mongo = await MongoMemoryServer.create();
-    const mongoUri = await mongo.getUri();
+// describe("create movies for users", () => {
+//   let mongo = "";
+//   beforeAll(async () => {
+//     process.env.API_KEY = "2c46c474";
+//     mongo = await MongoMemoryServer.create();
+//     const mongoUri = await mongo.getUri();
 
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  });
+//     await mongoose.connect(mongoUri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//   });
 
-  afterAll(async () => {
-    await mongo.stop();
-    await mongoose.connection.close();
-  });
+//   afterAll(async () => {
+//     await mongo.stop();
+//     await mongoose.connection.close();
+//   });
 
-  test("Should create a movie for authorized user with movie title", async () => {
-    const response = await request(app).post("/movies").send({
-      movieTitle: "No entry",
-    });
-    expect(response.statusCode).toBe(200);
+//   test("Should create a movie for authorized user with movie title", async () => {
+//     const response = await request(app).post("/movies").send({
+//       movieTitle: "No entry",
+//     });
+//     expect(response.statusCode).toBe(200);
+//   });
+// });
+
+let mongo = "";
+beforeAll(async () => {
+  process.env.API_KEY = "2c46c474";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+test("Should create a movie for authorized user with movie title", async () => {
+  const response = await request(app).post("/movies").send({
+    movieTitle: "No entry",
+  });
+  expect(response.statusCode).toBe(200);
 });
