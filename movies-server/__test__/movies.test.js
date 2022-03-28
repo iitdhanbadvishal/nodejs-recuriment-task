1842552fd9f188af17731823c19eb58105d0bc70
Vishal Anand <vishal.anand@qtsolv.com>
2022-03-28 12:12:36 +0530

    sdfsdfsdfsdf

--- movies-server/__test__/movies.test.js
+++ movies-server/__test__/movies.test.js
@@ -1,47 +1,9 @@
 const request = require("supertest");
 const app = require("../src/app");
-const { MongoMemoryServer } = require("mongodb-memory-server");
-const Movi = require("../src/models/movies");
-const mongoose = require("mongoose");
 
-describe("create movies for users", () => {
-  beforeAll(async () => {
-    process.env.API_KEY = "2c46c474";
-    mongo = await MongoMemoryServer.create();
-    const mongoUri = await mongo.getUri();
-
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  });
-
-  afterAll(async () => {
-    await mongo.stop();
-    await mongoose.connection.close();
-  });
-
-  test("Should create a movie for authorized user with movie title", async () => {
-    const movi = new Movi({
-      userId: "123",
-      title: "no entry",
-      released: "1222",
-      genre: "comedey",
-      director: "test",
-      createdAt: "111100020202",
-      month: "3",
-      year: "2022",
-    });
-    await movi.save();
-
-    await request(app)
-      .post("/movies")
-      .send({ movieTitle: "no entry" })
-      .expect(200);
-
-    // const response = await request(app).post("/movies").send({
-    //   movieTitle: "No entry",
-    // });
-    // expect(response.statusCode).toBe(200);
-  });
+it("Return 200 with successfull movie creation with given title", async () => {
+  return request(app)
+    .post("/movies")
+    .send({ movieTitle: "no entry" })
+    .expect(200);
 });
