b90b9c1cc850081db657801aa3da01d4524fcaa1
Vishal Anand <vishal.anand@qtsolv.com>
2022-03-28 18:02:35 +0530

    abc

--- movies-server/__test__/movies.test.js
+++ movies-server/__test__/movies.test.js
@@ -4,14 +4,21 @@ const app = require("../src/app");
 it("Return 200 with successfull movie creation with given title", async () => {
   const response = await await request(app)
     .post("/movies")
+    .set(
+      "authorization",
+      `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEyMywibmFtZSI6IkJhc2ljIFRob21hcyIsInJvbGUiOiJiYXNpYyIsImlhdCI6MTY0ODQ2OTg3NCwiZXhwIjoxNjQ4NDcxNjc0LCJpc3MiOiJodHRwczovL3d3dy5uZXRndXJ1LmNvbS8iLCJzdWIiOiIxMjMifQ.ALHgI01xWFFcpLlOVAxI_8O7W-6791-yrYbMhxoSfPo`
+    )
     .send({ movieTitle: "no entry" });
-
   expect(response.statusCode).toBe(200);
 });
 
 it("Return 400 when a movie title already exists", async () => {
   const response = await await request(app)
     .post("/movies")
+    .set(
+      "authorization",
+      `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEyMywibmFtZSI6IkJhc2ljIFRob21hcyIsInJvbGUiOiJiYXNpYyIsImlhdCI6MTY0ODQ2OTg3NCwiZXhwIjoxNjQ4NDcxNjc0LCJpc3MiOiJodHRwczovL3d3dy5uZXRndXJ1LmNvbS8iLCJzdWIiOiIxMjMifQ.ALHgI01xWFFcpLlOVAxI_8O7W-6791-yrYbMhxoSfPo`
+    )
     .send({ movieTitle: "no entry" });
 
   expect(response.statusCode).toBe(400);
@@ -20,6 +27,10 @@ it("Return 400 when a movie title already exists", async () => {
 it("Return 400 when user pass empty movie titile", async () => {
   const response = await await request(app)
     .post("/movies")
+    .set(
+      "authorization",
+      `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEyMywibmFtZSI6IkJhc2ljIFRob21hcyIsInJvbGUiOiJiYXNpYyIsImlhdCI6MTY0ODQ2OTg3NCwiZXhwIjoxNjQ4NDcxNjc0LCJpc3MiOiJodHRwczovL3d3dy5uZXRndXJ1LmNvbS8iLCJzdWIiOiIxMjMifQ.ALHgI01xWFFcpLlOVAxI_8O7W-6791-yrYbMhxoSfPo`
+    )
     .send({ movieTitle: "" });
 
   expect(response.statusCode).toBe(400);
--- movies-server/src/middleware/auth.js
+++ movies-server/src/middleware/auth.js
@@ -1,15 +1,19 @@
 const JWT = require("jsonwebtoken");
 const createError = require("http-errors");
+const Movie = require("../models/movies");
+const { getMonth, getYear } = require("../utils/utils");
 
 const { JWT_SECRET } = process.env;
 
 module.exports = {
   verifyAccessToken: (req, res, next) => {
+    console.log(req.headers["authorization"]);
     if (!req.headers["authorization"]) return next(createError.Unauthorized());
+    console.log("=========================================", JWT_SECRET);
     const authHeader = req.headers["authorization"];
     const bearerToken = authHeader.split(" ");
     const token = bearerToken[1];
-    JWT.verify(token, JWT_SECRET, (err, payload) => {
+    JWT.verify(token, process.env.JWT_SECRET, (err, payload) => {
       if (err) {
         const message =
           err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
@@ -18,5 +22,23 @@ module.exports = {
       req.payload = payload;
       next();
     });
+  },
+  roleIdentity: async (req, res, next) => {
+    const { userId, role } = req.payload;
+
+    if (role === "basic") {
+      let numberOfMovieCreatedPerMonth = await Movie.count({
+        userId: userId,
+        month: getMonth(),
+        year: getYear(),
+      });
+
+      if (numberOfMovieCreatedPerMonth >= 5) {
+        return res.status(400).json({
+          message: `You are a basic user, you can create only 5 movies per month`,
+        });
+      }
+    }
+    next();
   },
 };
