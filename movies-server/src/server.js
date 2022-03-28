f400cd3e6d817daf64ed161d8b70774ba3b50879
Vishal Anand <vishal.anand@qtsolv.com>
2022-03-28 13:01:50 +0530

    fsdfsdf

--- movies-server/src/server.js
+++ movies-server/src/server.js
@@ -1,8 +1,7 @@
-const app = require("./app")
+const app = require("./app");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
 
 const PORT = 3001;
 
@@ -18,20 +17,20 @@ if (!process.env.JWT_SECRET) {
   throw new Error("JWT_SECRET must be defined");
 }
 
-
 // db
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://vishal:vishal@cluster0.tuv3n.mongodb.net/nodeJsTask?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("**DB CONNECTED**"))
   .catch((err) => console.log("DB CONNECTION ERR => ", err));
 
 app.use(bodyParser.json());
 app.use(cors());
-
-
 
 app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
